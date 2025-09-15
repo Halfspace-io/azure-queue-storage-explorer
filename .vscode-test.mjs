e8f3874879f3c5f7710f724fc203c43257dadb66
Wojciech Jakub Ciok <108333442+wj-halfspace@users.noreply.github.com>
2025-09-15 16:21:05 +0200

    fix test

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -2,4 +2,10 @@ import { defineConfig } from '@vscode/test-cli';
 
 export default defineConfig({
 	files: 'out/test/**/*.test.js',
+	launchArgs: ['--disable-gpu', '--no-sandbox', '--disable-setuid-sandbox'],
+	extensionDevelopmentPath: '.',
+	extensionTestsPath: 'out/test',
+	version: '1.104.0',
+	platform: process.platform === 'win32' ? 'win32' : 'linux',
+	arch: process.arch === 'x64' ? 'x64' : 'ia32'
 });
